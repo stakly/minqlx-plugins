84f73fafa0a70ffe3808e3927dfaa714298cbbab
Stanislav Lyudvik <root@qw3rty.org>
2016-03-04 17:56:40 +0500

    Version 0.2, full core refactoring, added frag message

--- rdamage.py
+++ rdamage.py
@@ -7,23 +7,23 @@
 import minqlx
 from collections import defaultdict
 
-VERSION = "v0.1"
-SUPPORTED_GAMETYPES = ("ca", "dom", "ft")
+VERSION = 'v0.2'
+SUPPORTED_GAMETYPES = ('ca', 'dom', 'ft')
 
 
 class rdamage(minqlx.Plugin):
     def __init__(self):
         super().__init__()
 
-        self.add_hook("game_start", self.handle_game_start)
-        self.add_hook("round_start", self.handle_round_start)
-        self.add_hook("round_end", self.handle_round_end)
+        self.add_hook('game_start', self.handle_game_start)
+        self.add_hook('round_start', self.handle_round_start)
+        self.add_hook('round_end', self.handle_round_end)
+        self.add_hook('kill', self.handle_kill)
 
-        self.red_start = defaultdict(int)
-        self.blue_start = defaultdict(int)
+        self.allplayers = defaultdict(dict)
         self.game_supported = True
 
-    def handle_game_start(self, game):
+    def handle_game_start(self, data):
         try:
             gt = self.game.type_short
             if gt not in SUPPORTED_GAMETYPES:
@@ -32,62 +32,87 @@ class rdamage(minqlx.Plugin):
         except AttributeError:
             return minqlx.RET_STOP_ALL
 
-    def handle_round_start(self, data):
+    def handle_round_start(self, round_number):
         if not self.game_supported:
-            return
+            return minqlx.RET_STOP
 
         teams = self.teams()
-        red_start = teams["red"].copy()
-        blue_start = teams["blue"].copy()
-
-        for p in blue_start:
-            self.blue_start[p.name] = p.stats.damage_dealt
+        red_start = teams['red'].copy()
+        blue_start = teams['blue'].copy()
 
         for p in red_start:
-            self.red_start[p.name] = p.stats.damage_dealt
+            self.allplayers[p.name] = {}
+            self.allplayers[p.name]['team'] = 'red'
+            self.allplayers[p.name]['damage'] = p.stats.damage_dealt
+            self.allplayers[p.name]['frags'] = 0
+
+        for p in blue_start:
+            self.allplayers[p.name] = {}
+            self.allplayers[p.name]['team'] = 'blue'
+            self.allplayers[p.name]['damage'] = p.stats.damage_dealt
+            self.allplayers[p.name]['frags'] = 0
+
+    def handle_kill(self, victim, killer, data):
+        if not self.game_supported:
+            return minqlx.RET_STOP
+
+        if not data['WARMUP']:
+            self.allplayers[killer.name]['frags'] += 1
 
     def handle_round_end(self, data):
         if not self.game_supported:
-            return
+            return minqlx.RET_STOP
 
         teams = self.teams()
-        red_end = teams["red"].copy()
-        blue_end = teams["blue"].copy()
-        round_damage = defaultdict(int)
+        red_end = teams['red'].copy()
+        blue_end = teams['blue'].copy()
+        self.msg("^3*** ROUND {} END ***".format(data['ROUND']))
         try:
-            self.msg("^1RED SCORES: {}, PLAYERS ROUND DAMAGE:".format(self.game.red_score))
+            self.msg('^1RED SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.red_score))
             for p in red_end:
-                round_damage[p.name] = p.stats.damage_dealt-self.red_start[p.name]
-                if round_damage[p.name] >= 0:
-                    self.msg("^1  {0:<15} ^1: ^1{1}".format(p.name, round_damage[p.name]))
+                frags = self.allplayers[p.name]['frags']
+                frags_msg = ''
+                if frags > 0:
+                    end = 'S' if frags > 1 else ''
+                    frags_msg = ' ({} FRAG{})'.format(frags, end)
+                self.allplayers[p.name]['damage'] = p.stats.damage_dealt-self.allplayers[p.name]['damage']
+                if self.allplayers[p.name]['damage'] >= 0:
+                    self.msg('^1  {0:<15} ^1: ^1{1}{2}'.format(p.name, self.allplayers[p.name]['damage'], frags_msg))
         except AttributeError:
-            return minqlx.RET_STOP_ALL
+            return minqlx.RET_STOP
 
         try:
-            self.msg("^4BLUE SCORES: {}, PLAYERS ROUND DAMAGE:".format(self.game.blue_score))
+            self.msg('^4BLUE SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.blue_score))
             for p in blue_end:
-                round_damage[p.name] = p.stats.damage_dealt-self.blue_start[p.name]
-                if round_damage[p.name] >= 0:
-                    self.msg("^4  {0:<15} ^4: ^4{1}".format(p.name, round_damage[p.name]))
+                frags = self.allplayers[p.name]['frags']
+                frags_msg = ''
+                if frags > 0:
+                    end = 'S' if frags > 1 else ''
+                    frags_msg = ' ({} FRAG{})'.format(frags, end)
+                self.allplayers[p.name]['damage'] = p.stats.damage_dealt-self.allplayers[p.name]['damage']
+                if self.allplayers[p.name]['damage'] >= 0:
+                    self.msg('^4  {0:<15} ^4: ^4{1}{2}'.format(p.name, self.allplayers[p.name]['damage'], frags_msg))
         except AttributeError:
-            return minqlx.RET_STOP_ALL
+            return minqlx.RET_STOP
 
-        round_damage = sorted(round_damage.items(), key=lambda t: t[1], reverse=True)
-        n1player = round_damage[0][0]
-        damage = round_damage[0][1]
-        color = 7
-        for t in teams:
-            for p in teams[t]:
-                if t == "red":
-                    if p.name == n1player:
-                        color = 1
-                        break
-                if t == "blue":
-                    if p.name == n1player:
-                        color = 4
-                        break
-            else:
-                continue
-            break
+        n1player = next(iter (sorted(self.allplayers.items(), key=lambda x: x[1]['damage'], reverse=True)))
+        self.allplayers.clear()
+
+        nickname = n1player[0]
+        damage = n1player[1]['damage']
+        team = n1player[1]['team']
+        if team is 'red':
+            color = 1
+        elif team is 'blue':
+            color = 4
+        else:
+            color = 7
+
         if damage > 0:
-            self.msg("^3*** BEST ROUND PLAYER ^{}{} ^3WITH ^{}{} ^3DAMAGE ***^7".format(color, n1player, color, damage))
+            frags = n1player[1]['frags']
+            frags_msg = ''
+            if frags > 0:
+                end = 'S' if frags > 1 else ''
+                frags_msg = ' ^3WITH ^{}{} ^3FRAG{}'.format(color, frags, end)
+            self.msg('^3*** MOST DAMAGE ^{}{} ^3BY ^{}{}{} ^3***'
+                     .format(color, damage, color, nickname, frags_msg))
