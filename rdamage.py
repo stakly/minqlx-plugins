8cf2c18ad6719b5d07a70e8b651f5529ef46a967
Stanislav Lyudvik <root@qw3rty.org>
2016-03-09 13:15:00 +0500

    Code refactoring, exception handling

--- rdamage.py
+++ rdamage.py
@@ -7,7 +7,7 @@
 import minqlx
 from collections import defaultdict
 
-VERSION = '0.4'
+VERSION = '0.5'
 SUPPORTED_GAMETYPES = ('ca', 'dom', 'ft')
 
 
@@ -20,8 +20,10 @@ class rdamage(minqlx.Plugin):
         self.add_hook('round_end', self.handle_round_end)
         self.add_hook('kill', self.handle_kill)
 
-        self.allplayers = defaultdict(dict)
         self.game_supported = True
+        self.all_players = defaultdict(dict)
+        self.red_team = defaultdict(list)
+        self.blue_team = defaultdict(list)
 
     def handle_game_start(self, data):
         try:
@@ -36,15 +38,16 @@ class rdamage(minqlx.Plugin):
         if not self.game_supported:
             return minqlx.RET_STOP
 
-        self.allplayers.clear()
-        teams = self.teams()
-        red_start = teams['red'].copy()
-        blue_start = teams['blue'].copy()
+        self.all_players.clear()
 
-        for p in red_start:
+        teams = self.teams()
+        self.red_team = teams['red'].copy()
+        self.blue_team = teams['blue'].copy()
+
+        for p in self.red_team:
             self.fill_dict(p, 'red')
 
-        for p in blue_start:
+        for p in self.blue_team:
             self.fill_dict(p, 'blue')
 
     def handle_kill(self, victim, killer, data):
@@ -53,76 +56,80 @@ class rdamage(minqlx.Plugin):
 
         if not data['WARMUP']:
             try:
-                self.allplayers[killer.steam_id]['frags'] += 1
+                self.all_players[killer.steam_id]['frags'] += 1
             except KeyError:
-                return minqlx.RET_STOP
+                return
 
     def handle_round_end(self, data):
         if not self.game_supported:
             return minqlx.RET_STOP
 
-        teams = self.teams()
-        red_end = teams['red'].copy()
-        blue_end = teams['blue'].copy()
         self.msg("^3*** ROUND {} END ***".format(data['ROUND']))
+        self.msg('^1RED SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.red_score))
+        for p in self.red_team:
+            self.team_message(p)
 
-        try:
-            self.msg('^1RED SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.red_score))
-            for p in red_end:
-                self.team_message(p)
-        except AttributeError:
-            return minqlx.RET_STOP
+        self.msg('^4BLUE SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.blue_score))
+        for p in self.blue_team:
+            self.team_message(p)
 
-        try:
-            self.msg('^4BLUE SCORES: {}, PLAYERS ROUND DAMAGE:'.format(self.game.blue_score))
-            for p in blue_end:
-                self.team_message(p)
-        except AttributeError:
-            return minqlx.RET_STOP
-
-        leader = next(iter(sorted(self.allplayers.items(), key=lambda x: x[1]['damage'], reverse=True)))
-        looser = next(iter(sorted(self.allplayers.items(), key=lambda x: x[1]['damage'])))
+        leader = next(iter(sorted(self.all_players.items(), key=lambda x: x[1]['damage'], reverse=True)))
+        looser = next(iter(sorted(self.all_players.items(), key=lambda x: x[1]['damage'])))
         self.summary_message(leader, 'MOST DAMAGE')
         self.summary_message(looser, 'LEAST DAMAGE')
 
     def fill_dict(self, player, team):
         p = player
-        self.allplayers[p.steam_id] = {}
-        self.allplayers[p.steam_id]['name'] = p.clean_name
-        self.allplayers[p.steam_id]['team'] = team
-        self.allplayers[p.steam_id]['damage'] = p.stats.damage_dealt
-        self.allplayers[p.steam_id]['frags'] = 0
+        self.all_players[p.steam_id] = {}
+        self.all_players[p.steam_id]['name'] = p.clean_name
+        self.all_players[p.steam_id]['team'] = team
+        self.all_players[p.steam_id]['damage'] = p.stats.damage_dealt
+        self.all_players[p.steam_id]['frags'] = 0
 
     def team_message(self, player):
         p = player
-        frags = self.allplayers[p.steam_id]['frags']
-        frags_msg = ''
-        if frags > 0:
-            end = 'S' if frags > 1 else ''
-            frags_msg = ' ({} FRAG{})'.format(frags, end)
-        self.allplayers[p.steam_id]['damage'] = p.stats.damage_dealt-self.allplayers[p.steam_id]['damage']
-        damage = self.allplayers[p.steam_id]['damage']
-        if damage >= 0:
-            frags_msg = ' ^3(AFK?)' if damage == 0 else frags_msg
-            self.msg('^1  {0:<20}^1: ^1{1:<5}{2}'
-                     .format(p.clean_name, self.allplayers[p.steam_id]['damage'], frags_msg))
-
-    def summary_message(self, player_dict, text_prefix):
-        nickname = player_dict[1]['name']
-        damage = player_dict[1]['damage']
-        team = player_dict[1]['team']
-        if team is 'red':
-            color = 1
-        elif team is 'blue':
-            color = 4
-        else:
-            color = 7
-
-        if damage >= 0:
-            frags = player_dict[1]['frags']
-            frags_msg = ' ^3(AFK?)' if damage == 0 else ''
+        try:
+            frags = self.all_players[p.steam_id]['frags']
+            frags_msg = ''
             if frags > 0:
                 end = 'S' if frags > 1 else ''
-                frags_msg = ' ^3WITH ^{}{} ^3FRAG{}'.format(color, frags, end)
-            self.msg('^3*** {} ^{}{} ^3BY ^{}{}{} ^3***'
-                     .format(text_prefix, color, damage, color, nickname, frags_msg))
+                frags_msg = ' ({} FRAG{})'.format(frags, end)
+            self.all_players[p.steam_id]['damage'] = p.stats.damage_dealt-self.all_players[p.steam_id]['damage']
+            damage = self.all_players[p.steam_id]['damage']
+            if damage >= 0:
+                frags_msg = ' ^3(AFK?)' if damage == 0 else frags_msg
+                self.msg('^1  {0:<20}^1: ^1{1:<5}{2}'
+                         .format(p.clean_name, self.all_players[p.steam_id]['damage'], frags_msg))
+        except AttributeError as e:
+            self.logger.error("AttributeError: {}".format(e))
+            return
+        except KeyError as e:
+            self.logger.error("KeyError: {}".format(e))
+            return
+
+    def summary_message(self, player_dict, text_prefix):
+        try:
+            nickname = player_dict[1]['name']
+            damage = player_dict[1]['damage']
+            team = player_dict[1]['team']
+            if team is 'red':
+                color = 1
+            elif team is 'blue':
+                color = 4
+            else:
+                color = 7
+
+            if damage >= 0:
+                frags = player_dict[1]['frags']
+                frags_msg = ' ^3(AFK?)' if damage == 0 else ''
+                if frags > 0:
+                    end = 'S' if frags > 1 else ''
+                    frags_msg = ' ^3WITH ^{}{} ^3FRAG{}'.format(color, frags, end)
+                self.msg('^3*** {} ^{}{} ^3BY ^{}{}{} ^3***'
+                         .format(text_prefix, color, damage, color, nickname, frags_msg))
+        except AttributeError as e:
+            self.logger.error("AttributeError: {}".format(e))
+            return
+        except KeyError as e:
+            self.logger.error("KeyError: {}".format(e))
+            return
